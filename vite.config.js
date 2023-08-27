9d7f828f7ecbac47ffaff549ad489e691a4da198
Simon Gardette <sg@simon-gardette.fr>
2023-08-28 00:57:39 +0200

    add pwa

--- vite.config.js
+++ vite.config.js
@@ -5,19 +5,24 @@ import { imagetools } from "vite-imagetools";
 import { resolve, dirname } from "node:path";
 import { fileURLToPath } from "url";
 import VueI18nPlugin from "@intlify/unplugin-vue-i18n/vite";
+import { VitePWA } from "vite-plugin-pwa";
+
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
         vue(),
         imagetools(),
-        VueI18nPlugin({
-            /* options */
-            // locale messages resource pre-compile option
-            include: resolve(
-                dirname(fileURLToPath(import.meta.url)),
-                "./src/locales/**"
-            ),
-        }),
+        VueI18nPlugin(
+            {
+                /* options */
+                // locale messages resource pre-compile option
+                include: resolve(
+                    dirname(fileURLToPath(import.meta.url)),
+                    "./src/locales/**"
+                ),
+            },
+            VitePWA({ registerType: "autoUpdate" })
+        ),
     ],
 
     alias: {
